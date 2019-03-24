4e64e11ad775412d5c3ecd27214a03431db694a3
carlosCharlie <carlosc21c21@gmail.com>
2019-03-24 21:15:04 +0100

    limpiar archivos

--- proyecto/sample/datasets/descargaClimateKnowledge.r
+++ proyecto/sample/datasets/descargaClimateKnowledge.r
@@ -11,10 +11,20 @@ for(i in 1:(length(countries[,1]))){
   url <- paste("https://climateknowledgeportal.worldbank.org/api/data/get-download-data/historical/tas/1991-2016/",toupper(substr(countries[i,1],1,3)),"/",countries[i,1],sep="");
   print(url);
   tmp <- RCurl::getURL(url);
-  write.csv(tmp, file = paste("sample/datasets/climateKnowledge/",toupper( gsub(" ","_",countries[i,1])),"_temperature.csv"));
+  write(tmp, file = paste("sample/datasets/climateKnowledge/",toupper( gsub(" ","_",countries[i,1])),"_temperature.csv"));
   
   url <- paste("https://climateknowledgeportal.worldbank.org/api/data/get-download-data/historical/pr/1991-2016/",toupper(substr(countries[i,1],1,3)),"/",countries[i,1],sep="");
   print(url);
   tmp <- RCurl::getURL(url);
-  write.csv(tmp, file = paste("sample/datasets/climateKnowledge/",toupper( gsub(" ","_",countries[i,1])),"_rain.csv"));
+  write(tmp, file = gsub(" ","",paste("sample/datasets/climateKnowledge/",toupper( gsub(" ","_",countries[i,1])),"_rain.csv")));
 }
+
+#limpiando los vacios
+files <- list.files(path="sample/datasets/climateKnowledge",full.names = TRUE)
+lapply(files,function(x){
+  file <- read.csv2(x,sep=",",header = TRUE,stringsAsFactors = FALSE);
+  if(("Year" %in% colnames(file)) && length(file[,"Year"])==0){
+    print(paste("borrando",x));
+    file.remove(x);
+  }
+})
