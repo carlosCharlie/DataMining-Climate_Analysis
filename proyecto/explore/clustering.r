e695aa637d1fec5f17ec0c9df6d127860ebdd77b
carlosCharlie <carlosc21c21@gmail.com>
2019-04-01 20:32:15 +0200

    clustering con ejemplo (subido a la carpeta Explore)

--- proyecto/explore/clustering.r
+++ proyecto/explore/clustering.r
@@ -1,10 +1,12 @@
 
-temperaturas<-c(10:20);  #cambiar por un vector de TODAS las temperaturas
-lluvias<-c(20:30);       #cambiar por un vector de TODAS las lluvias
+temperature<-c(10:20);  #cambiar por un vector de TODAS las temperaturas
+raining<-c(20:30);       #cambiar por un vector de TODAS las lluvias
 
-temperatureRain <- matrix(c(temperaturas,lluvias),ncol=2); #Una columna para las temperaturas y otra para las lluvias.
+temperatureRain <- matrix(c(temperature,raining),ncol=2); #Una columna para las temperaturas y otra para las lluvias.
 result <- kmeans(temperatureRain,5)
 
 #para dibujar
 plot(temperatureRain,col=result$cluster)
-  
+
+for(i in 1:(length(temperature)))
+    print(paste("el clima del pais",i,"es de tipo",result$cluster[i]))
