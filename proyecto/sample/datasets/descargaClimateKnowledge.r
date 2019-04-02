6ed693a3fac9a0129a550c9451cb67532583b962
MrDevoid <josemanuelpintolozano@gmail.com>
2019-04-02 11:01:30 +0200

    Escritos objetivos y fase sample en la memoria

--- proyecto/sample/datasets/descargaClimateKnowledge.r
+++ proyecto/sample/datasets/descargaClimateKnowledge.r
@@ -19,14 +19,14 @@ for(i in 1:(length(countries[,1]))){
   
   print(as.character(countries[i,1]));
   
-  url <- paste("https://climateknowledgeportal.worldbank.org/api/data/get-download-data/historical/tas/1991-2016/",toupper(substr(countries[i,1],1,3)),"/",countries[i,1],sep="");
+  url <- paste("https://climateknowledgeportal.worldbank.org/api/data/get-download-data/historical/tas/1901-2016/",toupper(substr(countries[i,1],1,3)),"/",countries[i,1],sep="");
   print(url);
   tmp <- RCurl::getURL(url);
   if(nchar(tmp)>nchar("Temperature - (Celsius), Year, Month, Country, ISO3\n") && !grepl("<html>",tmp)){
   	#write(tmp, file = paste("sample/datasets/climateKnowledge/",toupper(gsub(" ","_",countries[i,1])),"_temperature.csv", sep=""));
   	df1 <- toDataFrame(tmp)
 
-  	url <- paste("https://climateknowledgeportal.worldbank.org/api/data/get-download-data/historical/pr/1991-2016/",toupper(substr(countries[i,1],1,3)),"/",countries[i,1],sep="");
+  	url <- paste("https://climateknowledgeportal.worldbank.org/api/data/get-download-data/historical/pr/1901-2016/",toupper(substr(countries[i,1],1,3)),"/",countries[i,1],sep="");
   	print(url);
   	tmp <- RCurl::getURL(url);
   	if(nchar(tmp)>nchar("Temperature - (Celsius), Year, Month, Country, ISO3\n") && !grepl("<html>",tmp)){
@@ -38,13 +38,3 @@ for(i in 1:(length(countries[,1]))){
   	}
   }
 }
-
-#limpiando los vacios
-files <- list.files(path="sample/datasets/climateKnowledge",full.names = TRUE)
-lapply(files,function(x){
-  file <- read.csv2(x,sep=",",header = TRUE,stringsAsFactors = FALSE);
-  if(("Year" %in% colnames(file)) && length(file[,"Year"])==0){
-    print(paste("borrando",x));
-    file.remove(x);
-  }
-})
