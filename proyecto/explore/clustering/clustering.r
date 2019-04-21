d7ba146e51387e85945ed3e9eebb20048c2d705c
carlosCharlie <carlosc21c21@gmail.com>
2019-04-22 01:23:32 +0200

    funcion predictClimante

--- proyecto/explore/clustering/clustering.r
+++ proyecto/explore/clustering/clustering.r
@@ -1,8 +1,8 @@
 
-#Ejecutar hasta que salga un grafico con los grupos bien diferenciados
+#Ejecutar todo el script hasta que salga un grafico con los grupos bien diferenciados
 source("explore/clustering/cargaFinalClustering.r")
 
-data <- loadTraining(30); #esta funcion esta en "cargaParaClustering.R"
+data <- loadTraining(30); #esta funcion esta en cargaFinalClustering.r
 
 temperature<-data$temperatures;
 raining<-data$raining;
@@ -20,3 +20,9 @@ plot(temperatureRain,col=result$cluster)
 
 for(i in 1:(length(temperature)))
     print(paste("el clima de ",gsub("-"," ",gsub("^([a-z,A-Z,-]*/)*|.csv","",data$names[i])),"es de tipo",result$cluster[i]))
+
+
+predictClimate <- function(temperature,raining){
+  require("clue")
+  clue::cl_predict(result,matrix(c(temperature,raining),ncol=2));
+}
