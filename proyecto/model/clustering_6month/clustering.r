c5c833e129f80a4d6922c7e1ca07cbdf53a4e3f2
carlosCharlie <carlosc21c21@gmail.com>
2019-05-07 21:30:43 +0200

    clustering 6 meses

--- proyecto/model/clustering_6month/clustering.r
+++ proyecto/model/clustering_6month/clustering.r
@@ -8,7 +8,7 @@
 
 rm(list=ls())
 # Ejecutar todo el script hasta que salga un grafico con los grupos bien diferenciados
-source("model/clustering/cargaNormalizada.r")
+source("model/clustering_6month/cargaNormalizada.r")
 
 # Esta función esta en cargaFinalClustering.r
 data <- loadTraining(70)
@@ -17,11 +17,10 @@ temperature<-data$temperatures;
 raining<-data$raining;
 names<-data$names;
 
-rm(data)
 rm(dataset)
 rm(datasets)
 
-temperatureRain <- matrix(c(temperature,raining),ncol=2); #Una columna para las temperaturas y otra para las lluvias.
+temperatureRain <- matrix(c(data$coldMonthsT,data$hotMonthsT,data$coldMonthsR,data$hotMonthsR),ncol=2); #Una columna para las temperaturas y otra para las lluvias.
 
 # Clustering jerárquico para ver cuantos grupos tiene sentido hacer
 plot(hclust(dist(temperatureRain)))
@@ -29,16 +28,16 @@ plot(hclust(dist(temperatureRain)))
 result <- kmeans(temperatureRain,4)
 
 # Para dibujar
-plot(temperatureRain,col=result$cluster)
+plot(matrix(c(temperature,raining),ncol=2),col=result$cluster)
 
 
-predictClimate <- function(temperature,raining){
+predictClimate <- function(coldMonthsT,hotMonthsT,coldMonthsR,hotMonthsR){
   
   if(!require("clue"))
     install.packages("clue");
   require("clue");
   
-  clue::cl_predict(result,matrix(c(temperature,raining),ncol=2));
+  clue::cl_predict(result,matrix(c(coldMonthsT,hotMonthsT,coldMonthsR,hotMonthsR),ncol=2));
 }
 
 # Elegimos países que tienen un tipo de clima claramente diferenciado y los utilizamos para agrupar los demás.
@@ -49,10 +48,10 @@ climateNames <- vector(mode = "character",length = 4);
 
 while(i<length(names) && length(climateNames[unlist(lapply(climateNames,function(x){is.na(x)||x==""}))])>0){
   
-  if(names[[i]]=="Panama")climateNames[predictClimate(temperature[i],raining[i])] = "tropical";
-  if(names[[i]]=="Saudi Arabia")climateNames[predictClimate(temperature[i],raining[i])] = "seco";
-  if(names[[i]]=="Italy")climateNames[predictClimate(temperature[i],raining[i])] = "moderado";
-  if(names[[i]]=="Canada")climateNames[predictClimate(temperature[i],raining[i])] = "continental";
+  if(names[[i]]=="Panama")climateNames[predictClimate(data$coldMonthsT[i],data$hotMonthsT[i],data$coldMonthsR[i],data$hotMonthsR[i])] = "tropical";
+  if(names[[i]]=="Saudi Arabia")climateNames[predictClimate(data$coldMonthsT[i],data$hotMonthsT[i],data$coldMonthsR[i],data$hotMonthsR[i])] = "seco";
+  if(names[[i]]=="Italy")climateNames[predictClimate(data$coldMonthsT[i],data$hotMonthsT[i],data$coldMonthsR[i],data$hotMonthsR[i])] = "moderado";
+  if(names[[i]]=="Canada")climateNames[predictClimate(data$coldMonthsT[i],data$hotMonthsT[i],data$coldMonthsR[i],data$hotMonthsR[i])] = "continental";
   
   i<-i+1;
 }
@@ -60,7 +59,7 @@ while(i<length(names) && length(climateNames[unlist(lapply(climateNames,function
 #formateo para que sea facil de usar luego
 climates <- vector();
 for(i in 1:(length(temperature))){
-    climates<-c(climates,(climateNames[result$cluster[i]]));
+  climates<-c(climates,(climateNames[result$cluster[i]]));
 }
 
 clustered <- data.frame(names=names,climate=climates)
